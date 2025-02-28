89eb2c53f1e2d6f0ebebff883b5b51e5773b1cdc
conradborchers <conrad.borchers@hotmail.de>
2025-02-27 22:07:09 -0500

    edm poster results v1

--- conrad-modeling.R
+++ conrad-modeling.R
@@ -34,10 +34,12 @@ d <- read_csv('Updated_LiveLab.csv') %>%
 
 # modeling:
 
+mins_agg <- "15 mins"
+
 # round and group by 10 min window
 d_log <- d %>%
   select(anon_student_id, cf_class_id, time, requested_help, got_help) %>%
-  mutate(time = round_date(time, "15 mins")) %>%
+  mutate(time = round_date(time, mins_agg)) %>%
   group_by(anon_student_id, cf_class_id, time) %>%
   summarize(
     requested_help = ifelse(sum(requested_help)>0, 1, 0),
@@ -46,7 +48,7 @@ d_log <- d %>%
   ungroup()
 
 d_detector_join <- d_detector %>%
-  mutate(time = round_date(time, "15 mins")) %>%
+  mutate(time = round_date(time, mins_agg)) %>%
   group_by(anon_student_id, time) %>%
   summarize(
     critical_struggle = ifelse(sum(critical_struggle)>0, 1, 0),
@@ -76,5 +78,103 @@ performance::icc(m, by_group = TRUE)
 m <- glmer(got_help ~ (1 | anon_student_id) + (1 | cf_class_id) + requested_help + idle, 
            d_model, family='binomial', verbose=2, nAGQ=0)
 
+d_model
+
 sjPlot::tab_model(m)
 performance::icc(m, by_group = TRUE)
+
+m <- glmer(got_help ~ (1 | anon_student_id) + (1 | cf_class_id) + requested_help + 
+             idle + system_misuse + struggle + student_doing_well, 
+           d_model, family='binomial', verbose=2, nAGQ=0)
+sjPlot::tab_model(m)
+performance::icc(m, by_group = TRUE)
+
+d_ranef <- ranef(m)$anon_student_id %>%
+  rownames_to_column('anon_student_id') %>%
+  tibble() %>%
+  janitor::clean_names()
+
+d_afm <- d %>%
+  filter(attempt_at_step==1) %>%
+  filter(!is.na(kc_model_mat_hia)) %>%
+  arrange(anon_student_id, time) %>%
+  select(anon_student_id, kc=kc_model_mat_hia, outcome) %>%
+  mutate(outcome = ifelse(outcome=='OK', 1, 0)) %>%
+  group_by(anon_student_id, kc) %>%
+  mutate(opportunity = 1:n()) %>%
+  ungroup()
+
+library(lme4)
+#iafm_form <- "outcome ~ opportunity + 
+#                 (1 | kc) +
+#                 (opportunity | anon_student_id)"
+#m_iafm <- glmer(iafm_form, d_afm, family=binomial(), verbose=2, nAGQ=0, control=glmerControl(optCtrl=list(rhoend=1e-4)))
+#summary(m_iafm)
+#iafm_ranef <- ranef(m_iafm)
+#saveRDS(iafm_ranef, 'iafm_ranef.rds')
+iafm_ranef <- readRDS('iafm_ranef.rds')
+d_join_iafm <- iafm_ranef$anon_student_id %>%
+  rownames_to_column('anon_student_id') %>%
+  tibble() %>%
+  janitor::clean_names() %>%
+  rename(initial_prof = intercept)
+
+d_corr <- d_ranef %>%
+  left_join(d_join_iafm, by='anon_student_id')
+
+cor.test(d_corr$intercept, d_corr$initial_prof)
+cor.test(d_corr$intercept, d_corr$opportunity)
+
+# individual OR, robustness
+
+# Convert results to a data frame
+samples <- replicate(1000,
+  {
+    d_model2 <- d_model[sample(nrow(d_model), replace = TRUE), ]
+    tbl <- xtabs(~got_help + requested_help, d_model2)
+    (tbl[2,2] * tbl[1,1]) / (tbl[1,2] * tbl[2,1])
+  }     
+)
+
+quantile(samples, c(0.025, 0.5, 0.975))
+
+samples <- replicate(1000,
+                     {
+                       d_model2 <- d_model[sample(nrow(d_model), replace = TRUE), ]
+                       tbl <- xtabs(~got_help + idle, d_model2)
+                       (tbl[2,2] * tbl[1,1]) / (tbl[1,2] * tbl[2,1])
+                     }     
+)
+
+quantile(samples, c(0.025, 0.5, 0.975))
+
+samples <- replicate(1000,
+                     {
+                       d_model2 <- d_model[sample(nrow(d_model), replace = TRUE), ]
+                       tbl <- xtabs(~got_help + system_misuse, d_model2)
+                       (tbl[2,2] * tbl[1,1]) / (tbl[1,2] * tbl[2,1])
+                     }     
+)
+
+quantile(samples, c(0.025, 0.5, 0.975))
+
+samples <- replicate(1000,
+                     {
+                       d_model2 <- d_model[sample(nrow(d_model), replace = TRUE), ]
+                       tbl <- xtabs(~got_help + struggle, d_model2)
+                       (tbl[2,2] * tbl[1,1]) / (tbl[1,2] * tbl[2,1])
+                     }     
+)
+
+quantile(samples, c(0.025, 0.5, 0.975))
+
+samples <- replicate(1000,
+                     {
+                       d_model2 <- d_model[sample(nrow(d_model), replace = TRUE), ]
+                       tbl <- xtabs(~got_help + student_doing_well, d_model2)
+                       (tbl[2,2] * tbl[1,1]) / (tbl[1,2] * tbl[2,1])
+                     }     
+)
+
+quantile(samples, c(0.025, 0.5, 0.975))
+
